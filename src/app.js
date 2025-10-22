a1f9c7773cc57d1516a36a0c067d27af9228b8d6
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2025-10-22 17:48:08 +0300

    creates a new nodejs app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = greet; 
+
+if (require.main === module) {
+    console.log(greet("World"));
+
+}
+
+
+// This code defines a simple greeting function and exports it for use in other modules.
+// // If the script is run directly, it will print a greeting to the console.
